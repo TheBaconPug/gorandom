348e064b93c79ca43e20d0ae1f51092875dd81fb
TheBaconPug <48840784+TheBaconPug@users.noreply.github.com>
2021-08-06 15:14:10 -0700

    Create gorandom.go

---
+++ gorandom.go
@@ -0,0 +1,51 @@
+package gorandom
+
+import (
+	"math/rand"
+	"time"
+	"unsafe"
+)
+
+const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+const numbers = "0123456789"
+
+const (
+	letterIdxBits = 6
+	letterIdxMask = 1<<letterIdxBits - 1
+	letterIdxMax  = 63 / letterIdxBits
+)
+
+func randInt(c int) int {
+	var src = rand.NewSource(time.Now().UnixNano())
+	b := make([]byte, c)
+	for i, cache, remain := c-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letters) {
+			b[i] = numbers[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+	return *(*int)(unsafe.Pointer(&b))
+}
+
+func randStr(c int) string {
+	var src = rand.NewSource(time.Now().UnixNano())
+	b := make([]byte, c)
+	for i, cache, remain := c-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letters) {
+			b[i] = letters[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+	return *(*string)(unsafe.Pointer(&b))
+}
